f988f1f6bd5ba64b4d3c65a244e4f3f2ada35095
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-08-29 15:59:13 +0900

    bug fix

--- models/auth.go
+++ models/auth.go
@@ -1,22 +1,16 @@
 package models
 
-import (
-	"fmt"
-
-	"github.com/golang-jwt/jwt/v4"
-)
-
 // 新規登録が成功したらトークンを発行してレスポンスに含める。
 
-func CreateJWTToken() {
-	// ヘッダー部分の作成
-	token := jwt.New(jwt.SigningMethodES256)
-	fmt.Printf("token: %v\n", token)
+// func CreateJWTToken() {
+// 	// ヘッダー部分の作成
+// 	token := jwt.New(jwt.SigningMethodES256)
+// 	fmt.Printf("token: %v\n", token)
 
-	// ペイロードの作成
-	token.Claims := jwt.MapClaims{
-	}
+// 	// ペイロードの作成
+// 	token.Claims := jwt.MapClaims{
+// 	}
 
-	// 署名を行う
+// 	// 署名を行う
 
-}
+// }
--- models/users.go
+++ models/users.go
@@ -1,20 +1,10 @@
 package models
 
-import (
-	"fmt"
-)
-
 // 値渡し
 func (u User) CreateUser() error {
 
-	fmt.Println("CreateUser!")
-	fmt.Printf("u: %+v\n", u)   // 値 ->
-	fmt.Printf("&u: %+v\n", &u) // ポインタ ->
-
 	// uのポインタ(アドレス)を渡す
 	if err := DB.Create(&u).Error; err != nil {
-		// SendResponse(w, err.Error(), http.StatusBadRequest)
-		// log.Println(err)
 		return err
 	}
 	return nil
