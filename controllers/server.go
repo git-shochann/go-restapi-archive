50e061e9dcf0de2a4a00155bc8cf0bedea3268ad
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-08-31 19:34:36 +0900

    wip signin

--- controllers/server.go
+++ controllers/server.go
@@ -95,6 +95,9 @@ func signupFunc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 重複時
+	// err.Error() -> "Error 1062: Duplicate entry 'test@gmail.com' for key 'users.email'"
+
 	// 成功！
 	// なぜ登録出来なかったのかもう少し詳細のメッセージがあっていいかも。
 	// errを使って "message": err など？
@@ -135,16 +138,23 @@ func signinFunc(w http.ResponseWriter, r *http.Request) {
 	// 結果を格納する構造体の生成
 	var user models.User
 
-	// emailでユーザーを検索する
-	err = models.GetUserByEmail(user, signinUser.Email)
+	// emailでユーザーを検索する -> 成功したらuserに値が入る
+	// user自体の実態を書き換えるのでアドレスを渡してあげる
+	err = models.GetUserByEmail(&user, signinUser.Email)
 	if err != nil {
 		models.SendErrorResponse(w, err.Error(), http.StatusInternalServerError)
 		log.Println(err)
 		return
 	}
+	// 実態を書き換えたそのままuserを出力する
+	fmt.Printf("user: %v\n", user) // {{1 2022-08-31 08:42:39 +0000 UTC 2022-08-31 08:42:39 +0000 UTC <nil>} test test test@gmail.com $2a$10$9QZC62Z6JODHtR1Kg1WCPuH9dAvbU64XJkJNlSwCIwlpbBQ84Eqxq}
 
 	// ここでログインユーザーを取得出来たのでuserを使ってく
 	// bcryptでDBはハッシュかしているので比較する関数
+
+	fmt.Printf("signinUser.Password: %v\n", signinUser.Password)
+	fmt.Printf("user.Password: %v\n", user.Password)
+
 	err = bcrypt.CompareHashAndPassword([]byte(signinUser.Password), []byte(user.Password))
 	if err != nil {
 		models.SendErrorResponse(w, err.Error(), http.StatusInternalServerError)
--- models/users.go
+++ models/users.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 // エラーハンドリング
 // https://gorm.io/ja_JP/docs/error_handling.html
 
@@ -16,12 +18,13 @@ func (u User) CreateUser() error {
 
 // Emailを元に重複していないか検索をする
 // User構造体の値はなぜ必要？ -> 結果を格納するため(out) -> First(out interface{}, where ...interface{}) *gorm.DB
-func GetUserByEmail(u User, email string) error {
+func GetUserByEmail(u *User, email string) error {
+	fmt.Printf("user1: %v\n", u) // user1: {{0 0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC <nil>}    }
 	// 検索する
-	if err := DB.Where("email = ?", email).First(&u).Error; err != nil {
+	if err := DB.Where("email = ?", email).First(u).Error; err != nil {
 		return err
 	}
 	// 結果の値を出力
-	// fmt.Printf("user: %v\n", u)
+	fmt.Printf("user2: %v\n", u) // user2: {{1 2022-08-31 08:42:39 +0000 UTC 2022-08-31 08:42:39 +0000 UTC <nil>} test test test@gmail.com $2a$10$9QZC62Z6JODHtR1Kg1WCPuH9dAvbU64XJkJNlSwCIwlpbBQ84Eqxq}
 	return nil
 }
